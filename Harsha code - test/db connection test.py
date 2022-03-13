e73dc7b95ad3c3c9d2927334de6cd9cd21d752ea
Harshavardhan Anbu <harsh.wiz@gmail.com>
2022-03-14 04:13:57 +0530

    commit

--- Harsha code - test/db connection test.py
+++ Harsha code - test/db connection test.py
@@ -2,5 +2,11 @@ import sqlalchemy
 import pymysql
 import sqlalchemy as db
 #get sqlalchemy and pymysql libraries
-print("sqlalchemy : {}".format(sqlalchemy.__version))
-#print("pymysql: {}".format(pymysql.__version__))
+print("sqlalchemy : {}".format(sqlalchemy.__version__))
+print("pymysql: {}".format(pymysql.__version__))
+#get engine objects using mypysql
+engine = db.create_engine("mysql+pymysql://root:Work@1234@localhost/harshadb")
+#get connection object
+connection = engine.connect()
+#get meta data object
+meta_data = db.MetaData()
